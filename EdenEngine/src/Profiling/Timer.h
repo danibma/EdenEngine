d2ff3255615df1885099eedd72b85247c378f3ce
Daniel Assunção <daniel.assuncao9@gmail.com>
2022-05-07 02:41:20 +0100

    added some profling stuff

--- EdenEngine/src/Profiling/Timer.h
+++ EdenEngine/src/Profiling/Timer.h
@@ -2,34 +2,37 @@
 
 #include <chrono>
 
-namespace Eden::Timer
+namespace Eden
 {
-	inline std::chrono::high_resolution_clock::time_point timestamp;
-
-	// Record a reference timestamp
-	inline void Record()
+	struct Timer
 	{
-		timestamp = std::chrono::high_resolution_clock::now();
-	}
+		std::chrono::high_resolution_clock::time_point timestamp;
 
-	// Elapsed time in seconds since the Timer creation or last call to record()
-	inline double ElapsedSeconds()
-	{
-		auto timestamp2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(timestamp2 - timestamp);
-		return time_span.count();
-	}
+		// Record a reference timestamp
+		inline void Record()
+		{
+			timestamp = std::chrono::high_resolution_clock::now();
+		}
 
-	// Elapsed time in milliseconds since the Timer creation or last call to record()
-	inline double ElapsedMilliseconds()
-	{
-		return ElapsedSeconds() * 1000.0;
-	}
+		// Elapsed time in seconds since the Timer creation or last call to record()
+		inline double ElapsedSeconds()
+		{
+			auto timestamp2 = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(timestamp2 - timestamp);
+			return time_span.count();
+		}
 
-	// Elapsed time in milliseconds since the Timer creation or last call to record()
-	inline double Elapsed()
-	{
-		return ElapsedMilliseconds();
-	}
+		// Elapsed time in milliseconds since the Timer creation or last call to record()
+		inline double ElapsedMilliseconds()
+		{
+			return ElapsedSeconds() * 1000.0;
+		}
 
+		// Elapsed time in milliseconds since the Timer creation or last call to record()
+		inline double Elapsed()
+		{
+			return ElapsedMilliseconds();
+		}
+
+	};
 }
--- EdenEngine/src/main.cpp
+++ EdenEngine/src/main.cpp
@@ -2,36 +2,69 @@
 #include "Core/Memory.h"
 #include "Core/Input.h"
 #include "Core/Log.h"
-#include <stdio.h>
+#include "Profiling/Timer.h"
+#include "Profiling/Profiler.h"
+
+#include <algorithm>
 
 using namespace Eden;
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+Window* window;
+
+// Timer stuff
+Timer timer;
+float deltaTime;
+
+void Init()
 {
 	Log::Init();
 
-	Window* window = enew Window("Eden Engine", 1600, 900);
+	window = enew Window("Eden Engine", 1600, 900);
+}
 
-	while (!window->IsCloseRequested())
+void Update()
+{
+	ED_PROFILE_FUNCTION();
+
+	window->UpdateEvents();
+
+	// Update timers
+	deltaTime = (float)timer.ElapsedSeconds();
+	timer.Record();
+
+	if (!window->IsMinimized())
 	{
-		window->UpdateEvents();
-
-		if (!window->IsMinimized())
+		if (Input::GetKeyDown(KeyCode::A))
 		{
-			if (Input::GetKeyDown(KeyCode::A))
-			{
-				ED_LOG_INFO("Key Pressed!");
-			}
-			else if(Input::GetKeyUp(KeyCode::A))
-			{
-				ED_LOG_INFO("Key Up!");
-			}
+			ED_LOG_INFO("Key Pressed!");
+		}
+		else if (Input::GetKeyUp(KeyCode::A))
+		{
+			ED_LOG_INFO("Key Up!");
 		}
 	}
+}
 
+void Destroy()
+{
 	edelete window;
 
 	Log::Shutdown();
+}
+
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	Init();
+
+	while (!window->IsCloseRequested())
+	{
+		ED_PROFILE_FRAME("MainThread");
+
+		Update();
+	}
+
+	Destroy();
 
 	return 0;
 }
