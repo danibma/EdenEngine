14f74eed5511ec142f5500589ce5c8acb4a19c4e
Daniel Assuncao <daniel.assuncao9@gmail.com>
2022-12-17 22:37:20 +0100

    removed submodules

---
+++ external/imgui/premake5.lua
@@ -0,0 +1,48 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/intermediate/")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+        "backends/imgui_impl_dx12.h",
+        "backends/imgui_impl_dx12.cpp",
+        "backends/imgui_impl_win32.h",
+        "backends/imgui_impl_win32.cpp",
+	}
+    
+    includedirs
+    {
+        "%{wks.location}/external/ImGui"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
---
+++ external/yaml-cpp/premake5.lua
@@ -0,0 +1,42 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/intermediate/")
+
+	files
+	{
+		"src/**.h",
+        "src/**.cpp",
+
+        "include/**.h",
+	}
+    
+    includedirs
+    {
+        "include"
+    }
+
+    defines
+    {
+        "YAML_CPP_STATIC_DEFINE"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
