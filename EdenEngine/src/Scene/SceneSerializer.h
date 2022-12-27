ae72c0ac06425aa1aa241a261309f13dfc96a13a
Daniel Assuncao <daniel.assuncao9@gmail.com>
2022-12-27 21:16:23 +0100

    remove unnecessary compile time

--- EdenEngine/src/Scene/SceneSerializer.h
+++ EdenEngine/src/Scene/SceneSerializer.h
@@ -2,66 +2,9 @@
 
 #include "Scene.h"
 
-#include <string>
-#include <fstream>
-#define YAML_CPP_STATIC_DEFINE
-#include <yaml-cpp/yaml.h>
-
-#include <glm/glm.hpp>
-
 namespace YAML
 {
-	// glm::vec3
-	template<>
-	struct convert<glm::vec3> 
-	{
-		static Node encode(const glm::vec3& rhs) 
-		{
-			Node node;
-			node.push_back(rhs.x);
-			node.push_back(rhs.y);
-			node.push_back(rhs.z);
-			return node;
-		}
-
-		static bool decode(const Node& node, glm::vec3& rhs) 
-		{
-			if (!node.IsSequence() || node.size() != 3)
-				return false;
-
-			rhs.x = node[0].as<float>();
-			rhs.y = node[1].as<float>();
-			rhs.z = node[2].as<float>();
-			return true;
-		}
-	};
-
-	// glm::vec4
-	template<>
-	struct convert<glm::vec4>
-	{
-		static Node encode(const glm::vec4& rhs)
-		{
-			Node node;
-			node.push_back(rhs.x);
-			node.push_back(rhs.y);
-			node.push_back(rhs.z);
-			node.push_back(rhs.w);
-			return node;
-		}
-
-		static bool decode(const Node& node, glm::vec4& rhs)
-		{
-			if (!node.IsSequence() || node.size() != 4)
-				return false;
-
-			rhs.x = node[0].as<float>();
-			rhs.y = node[1].as<float>();
-			rhs.z = node[2].as<float>();
-			rhs.w = node[3].as<float>();
-			return true;
-		}
-	};
+	class Emitter;
 }
 
 namespace Eden
